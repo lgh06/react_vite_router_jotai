a7a73d8da68e554b143a96a07aad3ea5cefbec88
Daniel <hnnk@qq.com>
2024-03-25 13:44:26 +0800

    简化atom

--- src/store/user.js
+++ src/store/user.js
@@ -1,40 +1,9 @@
 import { atom } from 'jotai'
 
-let userAtom = atom({
-  url: '',
-  loginKey: '',
-  userId: 0,
-  userName: "",
-});
 
-let urlAtom = atom((get) => {
-  const user = get(userAtom)
-  return user.url
-}, (get, set, update) => {
-  set(userAtom, (user) => ({ ...user, url: update }))
-})
+let urlAtom = atom("")
+let loginKeyAtom = atom("")
+let userIdAtom = atom(0)
+let userNameAtom = atom("")
 
-let loginKeyAtom = atom(
-  (get) => {
-    const user = get(userAtom)
-    return user.loginKey
-  },
-  (get, set, update) => {
-    set(userAtom, (user) => ({ ...user, loginKey: update }))
-  }
-)
-let userIdAtom = atom((get) => {
-  const user = get(userAtom)
-  return user.userId
-}, (get, set, update) => {
-  set(userAtom, (user) => ({ ...user, userId: update }))
-})
-let userNameAtom = atom((get) => {
-  const user = get(userAtom)
-  return user.userName
-}, (get, set, update) => {
-  set(userAtom, (user) => ({ ...user, userName: update }))
-})
-
-export default userAtom;
-export { userAtom, urlAtom, loginKeyAtom, userIdAtom, userNameAtom }
+export { urlAtom, loginKeyAtom, userIdAtom, userNameAtom }
